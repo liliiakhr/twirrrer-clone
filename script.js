861059eddea7944c770ebbd33c3b8d0b1746f3ca
liliiakhr <liliya.khrystyuk@gmail.com>
2021-05-05 15:33:18 +0200

    added js to post modal to blur modal button and "+"when the user is typing in the input field.

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const postBtn = document.querySelector(".post-btn");
 const modalWrapper = document.querySelector(".modal-wrapper");
 const modal = document.querySelector(".modal");
 const postModalX = document.querySelector(".modal-header i");
+const modalPostBtn = document.querySelector(".modal-header button");
+const modalFooterPlus = document.querySelector(".modal-footer span");
+const modalInput = document.querySelector(".modal-input");
 // **************************
 // **************************
 // Main page
@@ -61,7 +64,21 @@ postBtn.addEventListener("click", () => {
   modalWrapper.classList.add("modal-wrapper-display");
 });
 
+const changeOpacity = (x) => {
+  modalPostBtn.style.opacity = x;
+  modalFooterPlus.style.opacity = x;
+};
+
 postModalX.addEventListener("click", () => {
   modal.style.display = "none";
   modalWrapper.classList.remove("modal-wrapper-display");
+});
+
+modalInput.addEventListener("keypress", (e) => {
+  if (e.target.value !== "") {
+    changeOpacity(1);
+  }
+});
+modalInput.addEventListener("blur", (e) => {
+  if (e.target.value === "") changeOpacity(0.5);
 });
